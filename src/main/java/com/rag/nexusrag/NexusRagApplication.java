3ad8c30835091c6b20d8d951b3685062e67973ca
lee <2868226063@qq.com>
2026-09-06 13:02:28 +0800

    Initial commit

---
+++ src/main/java/com/rag/nexusrag/NexusRagApplication.java
@@ -0,0 +1,13 @@
+package com.rag.nexusrag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusRagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexusRagApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rag/nexusrag/NexusRagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rag.nexusrag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusRagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
